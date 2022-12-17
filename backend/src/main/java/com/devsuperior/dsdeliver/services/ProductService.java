782ecac52c233b997f96389cf6b432a2318a1a45
Andre Alencar <andrealencardosantos@gmail.com>
2022-12-17 15:54:30 -0300

    Creating endpoints PART 1

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -0,0 +1,18 @@
+package com.devsuperior.dsdeliver.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsdeliver.entities.Product;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+
+	public List<Product> findAll(){
+		
+	}
+}
